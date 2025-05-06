2efe3518fa2b0d937a170ce0ceac114d7b6a98a4
ucubz <ucubz@yahoo.com>
2025-05-06 20:45:03 +0700

    Update index.js
    ganti const port

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
